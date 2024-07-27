723aa0654a2e4fedfa8928030de62946bceb373c
RichiCod3s <108982619+RichiCod3s@users.noreply.github.com>
2024-07-27 17:11:23 +0800

    create getComputerChoice function/logic

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Hello World!")
+
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+
+function getComputerChoice(){
+
+    let choice = Math.floor(Math.random() * 3) +1;
+    if(choice ===1){
+        return rock;
+    }else if(choice ==2){
+        return paper;
+    }else{
+        return scissors;
+    }
+}
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice); // Outputs "Rock", "Paper", or "Scissors"
