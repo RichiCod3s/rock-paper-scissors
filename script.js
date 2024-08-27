2c661c76c93b5281c34e1fb5de3d8e746dde02dd
RichiCod3s <108982619+RichiCod3s@users.noreply.github.com>
2024-08-27 20:00:53 +0800

    fix disable buttons functionality and remove console logs

--- script.js
+++ script.js
@@ -16,12 +16,6 @@ function getComputerChoice(){
     }
 }
 
-//let computerChoice = getComputerChoice();
-//console.log(computerChoice); // Outputs "Rock", "Paper", or "Scissors"
-
-
-
-    //let choice = prompt("You feeling lucky? Rock, Paper or Scissors?");
    let humanChoice ='';
 
    const buttons = document.querySelectorAll('.rps-button');
@@ -47,45 +41,38 @@ function playRound(humanChoice, computerChoice){
     //Human loses logic
     if(humanChoice === "rock" && computerChoice === paper){
         document.querySelector('.round').textContent = "You lose!, Paper beats Rock";
-        console.log("You lose!, Paper beats Rock");
         computerScore++;
     }
 
     if(humanChoice === "paper" && computerChoice === scissors){
         document.querySelector('.round').textContent = "You lose!, Scissors beats Paper";
-        console.log("You lose!, Scissors beats Paper");
         computerScore++;
     }
 
     if(humanChoice === "scissors" && computerChoice === rock){
         document.querySelector('.round').textContent = "You lose!, Rock beats Scissors";
-        console.log("You lose!, Rock beats Scissors");
         computerScore++;
     }
 
     // human wins logic
     if(humanChoice === "rock" && computerChoice === scissors){
         document.querySelector('.round').textContent = "You win!, Rock beats Scissors";
-        console.log("You win!, Rock beats Scissors");
         humanScore++;
     }
 
     if(humanChoice === "paper" && computerChoice === rock){
         document.querySelector('.round').textContent = "You win!, Paper beats Rock";
-        console.log("You win!, Paper beats Rock");
         humanScore++;
     }
 
     if(humanChoice === "scissors" && computerChoice === paper){
         document.querySelector('.round').textContent = "You win!, Scissors beats Paper";
-        console.log("You win!, Scissors beats Paper");
         humanScore++;
     }  
 
     //draw 
     if(humanChoice === computerChoice){
         document.querySelector('.round').textContent = "It's a draw!";
-        console.log("It's a draw!");
     }
 }
 
@@ -94,19 +81,19 @@ function playRound(humanChoice, computerChoice){
 function playGame(){
         const humanSelection = humanChoice;
         const computerSelection = getComputerChoice();
+
         document.querySelector('.choice').textContent = "You chose " + humanSelection + 
         " & computer chose " + computerSelection;
-        console.log("You picked " + humanSelection);
-        console.log("Computer picks " + computerSelection);
+        
         playRound(humanSelection, computerSelection);
         document.querySelector('.score').textContent = "Your score: " + humanScore + " -- Computer's score: " + computerScore;
-        console.log("Your score: " + humanScore + " Computer's score: " + computerScore);
       
+        //display winner
         if(humanScore == 5) {
-            buttons.disabled =true;
+            buttons.forEach(button => button.disabled = true); 
             document.querySelector('.score').textContent = "You are the winner!"
         }else if(computerScore == 5){
-            buttons.disabled =true;
+            buttons.forEach(button => button.disabled = true); 
             document.querySelector('.score').textContent = "You lose, computer is the winner!"
         }
     
